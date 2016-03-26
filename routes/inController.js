fdb4e836634eb96e0b047b788a57efdf9a2a9a42
approce <seateq64@gmail.com>
2016-03-26 21:21:12 +0200

    Remove socket.io implementation.

--- routes/inController.js
+++ routes/inController.js
@@ -1,19 +1,8 @@
 var log    = require('winston');
-var socket = require('./socket');
 
 function onRequest(callback) {
-    socket.on('request', function (req) {
-        log.info('New request received.', req);
-
-        callback(req);
-    });
 }
 function onCommand(callback) {
-    socket.on('command', function (command) {
-        log.info('Received command from server.', command);
-
-        callback(command);
-    });
 }
 
 module.exports = {
--- routes/socket.js
+++
@@ -1,4 +0,0 @@
-var io        = require('socket.io-client');
-var serverUrl = require('../properties').serverUrl;
-
-module.exports = io.connect(serverUrl, {reconnect: true});
