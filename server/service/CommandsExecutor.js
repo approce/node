d1e1a92b46d21a0043237d7b8d782e1c45c12f8c
approce <seateq64@gmail.com>
2016-04-08 00:28:46 +0300

    Support getting commands for new created node.

--- server/service/CommandsExecutor.js
+++ server/service/CommandsExecutor.js
@@ -1,8 +1,8 @@
 var logger   = require('winston');
 var schedule = require('node-schedule');
 
-var nodesStorage   = require('./NodesStorage');
-var outCojntroller = require('../routes/outController');
+var nodesStorage  = require('./NodesStorage');
+var outController = require('../routes/outController');
 
 var repeatSchedule = 5; //min
 
@@ -12,7 +12,7 @@ function constructor() {
     var scheduled = false;
 
     //TODO do not listen to add event. Listen to a kind of 'number inited'
-    //nodesStorage.on('nodes:add', fetchCommand);
+    nodesStorage.on('nodes:add', processCommandForSingleNode);
 
     function scheduleFetches() {
         if (!scheduled) {
@@ -29,28 +29,27 @@ function constructor() {
 }
 
 function createScheduler() {
-    //TODO test:
-    var nodes = nodesStorage.get();
-    processCommands(nodes);
-
     schedule.scheduleJob('*/' + repeatSchedule + ' * * * *', function () {
-        logger.log('debug', 'Scheduled querying for commands');
-
         var nodes = nodesStorage.get();
+
+        logger.log('debug', 'Scheduled querying to server to get commands for nodes: %s', Object.keys(nodes));
+
         processCommands(nodes);
     });
+}
+
+function processCommandForSingleNode(node) {
+    logger.log('debug', 'Query to get command for new node id: %s', node.id);
+
+    var nodes      = {};
+    nodes[node.id] = node;
+    processCommands(nodes);
 }
 
 function processCommands(nodes) {
     var nodesIds = Object.keys(nodes);
 
-    fetchCommands(nodesIds).then(executeCommands.bind(null, nodes));
-}
-
-function fetchCommands(nodeIds) {
-    logger.log('debug', 'Querying to server to get commands for nodes: %s', nodeIds);
-
-    return outCojntroller.getCommands(nodeIds);
+    outController.getCommands(nodesIds).then(executeCommands.bind(null, nodes));
 }
 
 function executeCommands(nodes, commands) {
